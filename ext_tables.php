774b2a7f18dab4672f499540d3db519fae82abe8
Xavier Perseguers <xavier@causal.ch>
2017-03-17 10:31:15 +0100

    [BUGFIX] Prevent plugin from showing up in the list

--- ext_tables.php
+++ ext_tables.php
@@ -2,12 +2,6 @@
 defined('TYPO3_MODE') || die();
 
 $boot = function ($_EXTKEY) {
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-        'Causal.' . $_EXTKEY,
-        'Pi1',
-        'OpenID Connect'
-    );
-
     // Register TypoScript
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'OpenID Connect');
     if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('felogin')) {
