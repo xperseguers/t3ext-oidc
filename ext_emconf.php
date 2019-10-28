7e2e6cf648da222883da038184b203509c4ea297
Xavier Perseguers <xavier@causal.ch>
2019-10-28 09:47:23 +0100

    [TASK] Kickstart compatibility with TYPO3 v9
    We drop compatibility with TYPO3 v7 which is not supported anymore
    and we will migrate all database calls to Doctrine afterwards.

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,8 +21,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.4.0-dev',
     'constraints' => [
         'depends' => [
-            'php' => '5.5.0-7.3.99',
-            'typo3' => '7.6.0-8.99.99',
+            'php' => '7.0.0-7.3.99',
+            'typo3' => '8.7.0-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
