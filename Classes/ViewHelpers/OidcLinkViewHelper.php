c1c12a33e86be3cce00d01308dfa839228170120
Georg Ringer <georgringer@users.noreply.github.com>
2021-10-07 08:05:53 +0200

    [FEATURE] LinkViewHelper for felogin based on extbase
    If the extbased version of felogin is used, this ViewHelper can be used to retrieve the link

---
+++ Classes/ViewHelpers/OidcLinkViewHelper.php
@@ -0,0 +1,157 @@
+<?php
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace Causal\Oidc\ViewHelpers;
+
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Log\Logger;
+use TYPO3\CMS\Core\Log\LogManager;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
+
+class OidcLinkViewHelper extends AbstractViewHelper
+{
+
+    use CompileWithRenderStatic;
+
+    /**
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     * @return string link
+     */
+    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext)
+    {
+
+        $requestId = self::getUniqueId();
+        static::getLogger()->debug('Post-processing markers for felogin form', ['request' => $requestId]);
+        $link = '';
+
+        $typo3Branch = class_exists(Typo3Version::class)
+            ? (new Typo3Version())->getBranch()
+            : TYPO3_branch;
+        if (version_compare($typo3Branch, '9.0', '<')) {
+            $settings = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['oidc']);
+        } else {
+            $settings = $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['oidc'] ?? [];
+        }
+
+        if (empty($settings['oidcClientKey'])
+            || empty($settings['oidcClientSecret'])
+            || empty($settings['oidcEndpointAuthorize'])
+            || empty($settings['oidcEndpointToken'])
+        ) {
+            $link = 'Invalid OpenID Connect configuration';
+        } else {
+            if (session_id() === '') { // If no session exists, start a new one
+                static::getLogger()->debug('No PHP session found');
+                session_start();
+            }
+
+            if (empty($_SESSION['requestId']) || $_SESSION['requestId'] !== $requestId) {
+                self::prepareAuthorizationUrl($settings);
+                $_SESSION['requestId'] = $requestId;
+                $_SESSION['oidc_redirect_url'] = GeneralUtility::_GP('redirect_url');
+
+                static::getLogger()->debug('PHP session is available', [
+                    'id' => session_id(),
+                    'data' => $_SESSION,
+                ]);
+            } else {
+                static::getLogger()->debug('Reusing same authorization URL and state');
+            }
+
+            $link = $_SESSION['oidc_authorization_url'];
+        }
+
+        return $link;
+    }
+
+    /**
+     * Prepares the authorization URL and corresponding expected state (to mitigate CSRF attack)
+     * and stores information into the session.
+     *
+     * @param array $settings
+     * @return void
+     */
+    protected static function prepareAuthorizationUrl(array $settings)
+    {
+        /** @var \Causal\Oidc\Service\OAuthService $service */
+        $service = GeneralUtility::makeInstance(\Causal\Oidc\Service\OAuthService::class);
+        $service->setSettings($settings);
+        $authorizationUrl = $service->getAuthorizationUrl();
+
+        // Store the state
+        $state = $service->getState();
+
+        static::getLogger()->debug('Generating authorization URL', [
+            'url' => $authorizationUrl,
+            'state' => $state,
+        ]);
+
+        $loginUrl = GeneralUtility::getIndpEnv('TYPO3_REQUEST_URL');
+        // Sanitize the URL
+        $parts = parse_url($loginUrl);
+        $queryParts = array_filter(explode('&', $parts['query']), function ($v) {
+            [$k,] = explode('=', $v, 2);
+
+            return !in_array($k, ['logintype', 'tx_oidc[code]']);
+        });
+        $parts['query'] = implode('&', $queryParts);
+        $loginUrl = $parts['scheme'] . '://' . $parts['host'] . $parts['path'];
+        if (!empty($parts['query'])) {
+            $loginUrl .= '?' . $parts['query'];
+        }
+
+        $_SESSION['oidc_state'] = $state;
+        $_SESSION['oidc_login_url'] = $loginUrl;
+        $_SESSION['oidc_authorization_url'] = $authorizationUrl;
+    }
+
+    /**
+     * Returns a unique ID for the current processed request.
+     *
+     * This is supposed to be independent of the actual web server (Nginx or Apache) and
+     * the way PHP was built and unique enough for our use case, as opposed to using:
+     *
+     * - zend_thread_id() which requires PHP to be built with Zend Thread Safety - ZTS - support and debug mode
+     * - apache_getenv('UNIQUE_ID') which requires Apache as web server and mod_unique_id
+     *
+     * @return string
+     */
+    protected static function getUniqueId()
+    {
+        $uniqueId = sprintf('%08x', abs(crc32($_SERVER['REMOTE_ADDR'] . $_SERVER['REQUEST_TIME'] . $_SERVER['REMOTE_PORT'])));
+
+        return $uniqueId;
+    }
+
+    /**
+     * Returns a logger.
+     *
+     * @return Logger
+     */
+    protected static function getLogger()
+    {
+        /** @var Logger $logger */
+        static $logger = null;
+        if ($logger === null) {
+            $logger = GeneralUtility::makeInstance(LogManager::class)->getLogger(__CLASS__);
+        }
+
+        return $logger;
+    }
+}
