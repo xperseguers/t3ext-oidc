0ca5d6386490f589b176ed0bb79f0075643e44fa
Xavier Perseguers <xavier@causal.ch>
2023-02-09 22:36:01 +0100

    [TASK] Raise version to v1.2.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,10 +11,10 @@ $EM_CONF[$_EXTKEY] = [
     'createDirs' => '',
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.2.0-dev',
+    'version' => '1.2.0',
     'constraints' => [
         'depends' => [
-            'php' => '7.0.0-8.1.99',
+            'php' => '7.0.0-8.2.99',
             'typo3' => '8.7.0-11.5.99',
         ],
         'conflicts' => [],
