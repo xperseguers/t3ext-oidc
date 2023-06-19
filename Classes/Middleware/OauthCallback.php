767657ea8f909341dd87a8410a9d2a14c700d532
Markus Klein <markus.klein@reelworx.at>
2023-06-19 14:09:42 +0200

    [TASK] Migrate callback.php to Middleware
    The callback is handled as a generic middleware.
    The URL magic is gone and the current request
    is simply enriched with the specific page-type
    for further handling in the controller.
    
    The default redirectUri now is the startpage
    of the site.
    
    Resolves: #104

---
+++ Classes/Middleware/OauthCallback.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Causal\Oidc\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use TYPO3\CMS\Core\Http\Response;
+
+class OauthCallback implements MiddlewareInterface
+{
+    /**
+     * @param ServerRequestInterface $request
+     * @param RequestHandlerInterface $handler
+     * @return ResponseInterface
+     * see https://github.com/thephpleague/oauth2-client
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $queryParams = $request->getQueryParams();
+        $code = $queryParams['code'] ?? '';
+        if ($code) {
+            $state = $queryParams['state'] ?? '';
+            if (!$state) {
+                return new Response('Invalid state', 400);
+            }
+
+            $queryParams['type'] = 1489657462;
+            $request = $request->withQueryParams($queryParams);
+        }
+        return $handler->handle($request);
+    }
+}
