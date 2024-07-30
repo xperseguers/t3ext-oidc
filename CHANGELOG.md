e2432e5c77dc1baf7042be4cc252474df393573b
Markus Klein <markus.klein@reelworx.at>
2024-07-30 11:17:26 +0200

    [FEATURE] Add event to modify where-conditions for fe_users
    Resolves: #164

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,5 +2,6 @@
 
 ## Version 2.x
 
+* Added an event allowing to adjust the where-conditions for fetching the existing fe_users [#164](https://github.com/xperseguers/t3ext-oidc/issues/164)
 * Enhanced events to include a reference to the AuthenticationService [#136](https://github.com/xperseguers/t3ext-oidc/issues/136)
 * Added a user groups event to map groups by a different pattern than "Roles", e.g. "claims" [#129](https://github.com/xperseguers/t3ext-oidc/pull/129)
---
+++ Classes/Event/AuthenticationFetchUserEvent.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace Causal\Oidc\Event;
+
+use Doctrine\DBAL\Query\Expression\CompositeExpression;
+use TYPO3\CMS\Core\Authentication\AbstractAuthenticationService;
+use TYPO3\CMS\Core\Database\Query\QueryBuilder;
+
+final class AuthenticationFetchUserEvent
+{
+    protected array $resourceInfo;
+
+    /**
+     * @var array<string|CompositeExpression>
+     */
+    protected array $conditions;
+
+    protected QueryBuilder $queryBuilder;
+
+    protected AbstractAuthenticationService $authenticationService;
+
+    public function __construct(
+        array $resourceInfo,
+        array $conditions,
+        QueryBuilder $queryBuilder,
+        AbstractAuthenticationService $authenticationService
+    ) {
+        $this->resourceInfo = $resourceInfo;
+        $this->conditions = $conditions;
+        $this->queryBuilder = $queryBuilder;
+        $this->authenticationService = $authenticationService;
+    }
+
+    public function getResourceInfo(): array {
+        return $this->resourceInfo;
+    }
+
+    /**
+     * @return array<CompositeExpression|string>
+     */
+    public function getConditions(): array
+    {
+        return $this->conditions;
+    }
+
+    public function setConditions(array $conditions): void
+    {
+        $this->conditions = $conditions;
+    }
+
+    public function getQueryBuilder(): QueryBuilder
+    {
+        return $this->queryBuilder;
+    }
+
+    public function getAuthenticationService(): AbstractAuthenticationService
+    {
+        return $this->authenticationService;
+    }
+}
