b790abea8ce29433e1c96d865a04f4e10b5304b6
Cyril Janody <cyril.janody@fsg.ulaval.ca>
2025-08-04 14:11:50 -0400

    Replace the extension icon with an SVG version and register it in the registry

---
+++ Configuration/Icons.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'ext-oidc-icon' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:oidc/Resources/Public/Icons/Extension.svg',
+    ],
+];
