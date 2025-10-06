b5795fb62ff77f8015d7124ac6b2a0bfa5618691
Hannes Lau <office@hanneslau.de>
2025-10-06 22:31:23 +0200

    [TASK] Extend acceptance tests to v12 instance

--- Build/playwright/e2e/tests/two-consecutive-logins.test.ts
+++ Build/playwright/e2e/tests/two-consecutive-logins.test.ts
@@ -1,17 +1,17 @@
 import { test, expect } from '@playwright/test';
 
 test('Login via login form', async ({ page }) => {
-  await page.goto('https://v13.t3ext-oidc.test/');
+  await page.goto('/');
   await page.getByRole('link', { name: 'Login with OpenID Connect' }).click();
   await expect(page).toHaveURL(new RegExp('http://oidc.t3ext-oidc.test/'));
 
   await page.getByLabel('Username').fill('User1');
   await page.getByLabel('Password').fill('pwd');
   await page.getByRole('button', {name: 'Login'}).click()
-  await expect(page).toHaveURL(new RegExp('https://v13.t3ext-oidc.test/'));
+  await expect(page).toHaveURL(new RegExp('/'));
   await expect(page.getByText('You are now logged in as \'1\'')).toBeVisible();
 
-  await page.goto('https://v13.t3ext-oidc.test/en/');
+  await page.goto('/en/');
   await expect(page.getByText('Username 1')).toBeVisible();
 
   await page.getByRole('button', {name: 'Logout'}).click();
@@ -20,7 +20,7 @@ test('Login via login form', async ({ page }) => {
   await page.getByRole('link', { name: 'Login with OpenID Connect' }).click();
   await expect(page.getByText('You are now logged in as \'1\'')).toBeVisible();
 
-  await page.goto('https://v13.t3ext-oidc.test/en/');
+  await page.goto('/en/');
   await expect(page.getByText('Username 1')).toBeVisible();
 
   await page.getByRole('button', {name: 'Logout'}).click();
