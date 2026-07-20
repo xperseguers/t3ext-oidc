23aca5ca479edfd9a770aab39ee57d03f7a1ed4d
Markus Klein <markus.klein@reelworx.at>
2026-07-20 08:16:11 +0200

    [TASK] Update scripts

--- scripts/runphptool.sh
+++ scripts/runphptool.sh
@@ -1,19 +1,47 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+containercommand=""
+if [[ -x $(which "docker") ]]; then
+    containercommand="docker compose"
+fi
+if [[ -x $(which "podman") ]]; then
+    containercommand="podman compose"
+fi
 if [[ -x $(which "podman-compose") ]]; then
-    composecommand="podman-compose"
-else
-    if [[ -x $(which "podman") ]]; then
-        composecommand="podman compose"
-    else
-        composecommand="docker compose"
-    fi
+    containercommand="podman-compose"
 fi
 
+echo "### Using this container command: $containercommand"
+
+composercommand="bash -c"
+phpcommand="php -d memory_limit=512M"
+if [[ -n $containercommand ]]; then
+    composercommand="$containercommand run --rm tools bash -c"
+    phpcommand="$containercommand run --rm tools $phpcommand"
+fi
+
+export PHP_VERSION
+
 mkdir -p $TOOL_DIR
-$composecommand run --rm tools composer req --working-dir=$TOOL_DIR --dev -W $TOOL_PACKAGE
-$composecommand run --rm tools $TOOL_DIR/vendor/bin/$TOOL_COMMAND "$@"
+
+echo "### Installing tool"
+read -r -d '' composer_setup <<- EOM || true
+    if [[ ! -f $TOOL_DIR/composer.json ]]; then
+      composer init --working-dir=$TOOL_DIR --no-interaction --stability=stable --name="rx/tool"
+      composer config --working-dir=$TOOL_DIR --no-interaction allow-plugins true
+      composer config --working-dir=$TOOL_DIR --no-interaction lock false
+    fi
+    composer req --working-dir=$TOOL_DIR --dev -a -W $TOOL_PACKAGE
+EOM
+$composercommand "$composer_setup"
+
+echo "### Running tool"
+$phpcommand $TOOL_DIR/vendor/bin/$TOOL_COMMAND "$@"
 result=$?
 
-$composecommand down
+echo "### Tearing down containers"
+[[ -z $containercommand ]] || $containercommand down
+
 exit $result
