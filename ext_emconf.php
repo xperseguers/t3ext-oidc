181d9adb47383171ff9045d91f516e0adf360d4a
Markus Klein <markus.klein@reelworx.at>
2026-07-24 21:27:06 +0200

    [TASK] Adjust ext_emconf.php to match composer.json

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '5.0.0',
     'constraints' => [
         'depends' => [
-            'php' => '8.1.0-8.5.99',
+            'php' => '8.2.0-8.5.99',
             'typo3' => '12.4.33-13.4.99',
         ],
         'conflicts' => [],
