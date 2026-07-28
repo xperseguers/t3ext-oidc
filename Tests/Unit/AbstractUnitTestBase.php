3c71af22432ad5308c40af760183bfdc0803f994
Cyril Janody <cyril.janody@fsg.ulaval.ca>
2026-07-28 15:57:04 -0400

    Tests

--- Tests/Unit/AbstractUnitTestBase.php
+++ Tests/Unit/AbstractUnitTestBase.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Causal\Oidc\Tests\Unit;
 
+use Causal\Oidc\Exception\ExtensionNotConfiguredException;
 use Causal\Oidc\OidcConfiguration;
 use Causal\Oidc\Service\OAuthService;
 use Psr\EventDispatcher\EventDispatcherInterface;
@@ -27,38 +28,50 @@ class AbstractUnitTestBase extends UnitTestCase
 
     /**
      * @throws ExtensionConfigurationPathDoesNotExistException
-     * @throws ExtensionConfigurationExtensionNotConfiguredException
+     * @throws ExtensionNotConfiguredException
      */
     protected function setupOidcConfiguration(): OidcConfiguration
     {
         return new OidcConfiguration([
-            'enableBackendAuthentication' => 0,
-            'enableFrontendAuthentication' => 0,
-            'reEnableFrontendUsers' => 0,
-            'undeleteFrontendUsers' => 0,
-            'frontendUserMustExistLocally' => 0,
-            'enableCodeVerifier' => 0,
-            'enablePasswordCredentials' => 0,
-            'usersStoragePid' => 0,
-            'usersDefaultGroup' => '',
-            'redirectUri' => '',
-            'clientKey' => '',
-            'clientSecret' => '',
-            'clientScopes' => 'openid',
-            'clientScopeSeparator' => '',
-            'endpointAuthorize' => '',
-            'endpointToken' => '',
-            'endpointUserInfo' => '',
-            'endpointLogout' => '',
-            'endpointRevoke' => '',
-            'authorizeLanguageParameter' => 'language',
-            'useRequestPathAuthentication' => 0,
-            'revokeAccessTokenAfterLogin' => 0,
-            'disableCSRFProtection' => 0,
-            'oauthProviderFactory' => '',
             'authenticationServicePriority' => 82,
             'authenticationServiceQuality' => 80,
             'authenticationUrlRoute' => 'oidc/authentication',
+            'providers' => [
+                'default' => [
+                    'enableBackendAuthentication' => 1,
+                    'enableFrontendAuthentication' => 1,
+                    'reEnableFrontendUsers' => 0,
+                    'undeleteFrontendUsers' => 0,
+                    'frontendUserMustExistLocally' => 0,
+                    'enableCodeVerifier' => 0,
+                    'enablePasswordCredentials' => 0,
+                    'usersStoragePid' => 0,
+                    'usersDefaultGroup' => '',
+                    'redirectUri' => '',
+                    'clientKey' => 't3ext-oidc',
+                    'clientSecret' => 't3ext-oidc',
+                    'clientScopes' => 'openid',
+                    'clientScopeSeparator' => '',
+                    'endpointAuthorize' => 'http://oidc.t3ext-oidc.test/connect/authorize',
+                    'endpointToken' => 'http://oidc.t3ext-oidc.test/connect/token',
+                    'endpointUserInfo' => 'http://oidc.t3ext-oidc.test/connect/userinfo',
+                    'endpointLogout' => '',
+                    'endpointRevoke' => 'http://oidc.t3ext-oidc.test/connect/revocation',
+                    'authorizeLanguageParameter' => 'language',
+                    'useRequestPathAuthentication' => 0,
+                    'revokeAccessTokenAfterLogin' => 0,
+                    'disableCSRFProtection' => 0,
+                    'oauthProviderFactory' => '',
+                    'mapping' => [
+                        'be_users' => [
+                            'realName' => '<name>',
+                        ],
+                        'fe_users' => [
+                            'name' => '<name>',
+                        ],
+                    ],
+                ],
+            ],
         ]);
     }
 }
