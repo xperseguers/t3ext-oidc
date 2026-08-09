d6718d90ee995d2553566da8433450bd97db93be
Markus Klein <markus.klein@reelworx.at>
2026-08-09 22:18:22 +0200

    [BUGFIX] Correct path for BE login view Fluid

--- Classes/LoginProvider/OidcLoginProvider14.php
+++ Classes/LoginProvider/OidcLoginProvider14.php
@@ -7,6 +7,7 @@ namespace Causal\Oidc\LoginProvider;
 use Psr\Http\Message\ServerRequestInterface;
 use TYPO3\CMS\Backend\LoginProvider\LoginProviderInterface;
 use TYPO3\CMS\Core\View\ViewInterface;
+use TYPO3\CMS\Fluid\View\FluidViewAdapter;
 
 class OidcLoginProvider14 implements LoginProviderInterface
 {
@@ -18,6 +19,9 @@ class OidcLoginProvider14 implements LoginProviderInterface
     public function modifyView(ServerRequestInterface $request, ViewInterface $view): string
     {
         $view->assign('enablePasswordReset', false);
+        if ($view instanceof FluidViewAdapter) {
+            $view->getRenderingContext()->getTemplatePaths()->setTemplateRootPaths(['EXT:oidc/Resources/Private/Templates/']);
+        }
         return 'Backend/LoginOidc';
     }
 }
