16bb988568081f51757553e630fc23bcaefec6e7
Markus Klein <markus.klein@reelworx.at>
2026-08-10 11:07:50 +0200

    [TASK] Combine OidcLoginProvider classes again
    By removing the strict type in the old render-method, it can now be empty.

--- Classes/LoginProvider/OidcLoginProvider.php
+++ Classes/LoginProvider/OidcLoginProvider.php
@@ -8,7 +8,6 @@ use Psr\Http\Message\ServerRequestInterface;
 use TYPO3\CMS\Backend\Controller\LoginController;
 use TYPO3\CMS\Backend\LoginProvider\LoginProviderInterface;
 use TYPO3\CMS\Core\Page\PageRenderer;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\View\ViewInterface;
 use TYPO3\CMS\Fluid\View\FluidViewAdapter;
 
@@ -17,6 +16,10 @@ class OidcLoginProvider implements LoginProviderInterface
     public const int IDENTIFIER = 1742888452490;
 
     /**
+     * This method is only here, because the Interface in v13
+     * requires it. But the Core actually never uses this method
+     * as long as modifyView is present.
+     *
      * @inheritDoc
      * @param \TYPO3\CMS\Fluid\View\StandaloneView $view
      */
